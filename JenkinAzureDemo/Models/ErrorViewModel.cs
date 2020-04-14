18300e70aecf8745655687f9750b4dfc0d3479cd
unknown <radhesah@gmail.com>
2020-04-14 11:24:34 +0000

    first commit

---
+++ JenkinAzureDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JenkinAzureDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
